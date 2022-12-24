8d7a0aa52af373e7ccafea6b25b7740c073f3cf0
Giorgos Stefanakis <stefgs99@gmail.com>
2022-12-24 13:17:09 +0000

    Progress

--- backend/src/entities/noobcash-node.ts
+++ backend/src/entities/noobcash-node.ts
@@ -4,11 +4,11 @@ import ITransaction from "../interfaces/transaction.interface";
 import IWallet from "../interfaces/wallet.interface";
 
 export default abstract class NoobcashNode {
-    public abstract generateWallet(): IWallet;
-    public abstract createTransaction(): ITransaction;
-    public abstract signTransaction(transaction: ITransaction): void;
-    public abstract broadcastTransaction(transaction: ITransaction): void;
-    public abstract verifySignature(transaction: ITransaction): void;
+    public abstract generateWallet(): IWallet; // OK
+    public abstract createTransaction(): ITransaction; // OK
+    public abstract signTransaction(transaction: ITransaction): void; // OK
+    public abstract broadcastTransaction(transaction: ITransaction): void; 
+    public abstract verifySignature(transaction: ITransaction): void; // OK
     public abstract validateTransaction(transaction: ITransaction): void;
     public abstract getWalletBalance(wallet: IWallet): void;
     public abstract mineBlock(block: IBlock): void;
