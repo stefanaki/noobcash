dbaf8d6c28054fb961a9e3a4d816726f275e4968
Giorgos Stefanakis <stefgs99@gmail.com>
2022-12-26 21:21:44 +0000

    Transaction service progress

--- backend/src/services/transaction.service.ts
+++ backend/src/services/transaction.service.ts
@@ -1,13 +1,118 @@
+import { sign, verify } from 'crypto';
+import logger from '../utilities/logger';
+import config from '../config';
+import Transaction from '../entities/transaction.entity';
+import { IUnspentTransactionOutput } from '../interfaces/transaction.interface';
+import TransactionInput from '../entities/transaction-input.entity';
+import TransactionOutput from '../entities/transaction-output.entity';
+
 export default class TransactionService {
-    private static instance: TransactionService;
+	private static instance: TransactionService;
 
-    private constructor() {}
+	private constructor() {}
 
-    public static getInstance(): TransactionService {
-        if (!TransactionService.instance) {
-            TransactionService.instance = new TransactionService();
-        }
+	public static getInstance(): TransactionService {
+		if (!TransactionService.instance) {
+			TransactionService.instance = new TransactionService();
+		}
 
-        return this.instance;
-    }
+		return this.instance;
+	}
+
+	verifySignature(t: Transaction): boolean {
+		if (!t.signature) return false;
+
+		const signatureData = {
+			transactionId: t.transactionId,
+			senderAddress: t.senderAddress,
+			receiverAddress: t.receiverAddress,
+			amount: t.amount
+		};
+
+		try {
+			verify(
+				'sha256',
+				Buffer.from(JSON.stringify(signatureData)),
+				t.senderAddress,
+				Buffer.from(t.signature)
+			);
+
+			return true;
+		} catch {
+			return false;
+		}
+	}
+
+	signTransaction(t: Transaction, privateKey: string) {
+		const signatureData = {
+			transactionId: t.transactionId,
+			senderAddress: t.senderAddress,
+			receiverAddress: t.receiverAddress,
+			amount: t.amount
+		};
+		t.signature = sign('sha256', Buffer.from(JSON.stringify(signatureData)), {
+			key: privateKey,
+			passphrase: config.passphrase
+		});
+	}
+
+	findTransactionInputs(
+		t: Transaction,
+		utxoList: IUnspentTransactionOutput
+	): {
+		newTransactionInputs: TransactionInput[];
+		toBeSpentUtxos: TransactionOutput[];
+		totalUtxoAmount: number;
+	} | null {
+		let totalUtxoAmount = 0;
+		let toBeSpentUtxos: TransactionOutput[] = [];
+
+		for (let utxo of utxoList.utxos) {
+			if (totalUtxoAmount >= t.amount) break;
+
+			totalUtxoAmount += utxo.amount;
+			toBeSpentUtxos.push(utxo);
+		}
+
+		if (totalUtxoAmount < t.amount) {
+			logger.error(`Transaction ${t.transactionId} failed, not enough coins`);
+			return null;
+		}
+
+		logger.info(
+			`Found enough UTXO's to fullfill transaction ${t.transactionId}`
+		);
+
+		let newTransactionInputs = toBeSpentUtxos.map(
+			(utxo): TransactionInput =>
+				new TransactionInput(utxo.outputId, utxo.amount)
+		);
+
+		return {
+			newTransactionInputs,
+			toBeSpentUtxos,
+			totalUtxoAmount
+		};
+	}
+
+	createTransactionOutputs(
+		t: Transaction,
+		totalUtxoAmount: number
+	): TransactionOutput[] {
+		let newTransactionOutputs: TransactionOutput[] = [
+			new TransactionOutput(t.transactionId, t.receiverAddress, t.amount)
+		];
+		
+		if (totalUtxoAmount > t.amount) {
+			newTransactionOutputs.push(
+				new TransactionOutput(
+					t.transactionId,
+					t.senderAddress,
+					totalUtxoAmount - t.amount
+				)
+			);
+		}
+
+		return newTransactionOutputs;
+	}
 }
